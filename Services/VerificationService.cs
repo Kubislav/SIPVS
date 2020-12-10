8f6314c87d0e3192877e9bf7d14ceebfd345a704
stefan35 <stefan2640@gmail.com>
2020-12-10 01:52:59 +0100

    pridanie dalsich overeni

--- Services/VerificationService.cs
+++ Services/VerificationService.cs
@@ -19,6 +19,9 @@ using Org.BouncyCastle.Tsp;
 using Org.BouncyCastle.X509.Store;
 using Org.BouncyCastle.Cms;
 using Org.BouncyCastle.Asn1.Tsp;
+using System.Security.Cryptography.X509Certificates;
+using LanguageExt;
+using System.Numerics;
 
 namespace SIPVS
 {
@@ -31,7 +34,7 @@ namespace SIPVS
             Array.Sort(files);
             foreach(string filename in files) 
             {
-                report += $" * {filename.Split('/')[2]}: \n\t* {verify(filename)}\n\n";
+                report += $" * {filename.Split('\\')[1]}: \n\t* {verify(filename)}\n\n";
                 System.Console.WriteLine(filename);
             }
             return report;
@@ -75,7 +78,6 @@ namespace SIPVS
             {
                 return "OK";
             } 
-
 
             return "koreňový element musí obsahovať atribúty xmlns:xzep a xmlns:ds podľa profilu XADES_ZEP";
         }
@@ -133,7 +135,141 @@ namespace SIPVS
         
         private string step_3(string filename) //TODO: Core validation
         {
-            return "TODO: dalsie body overenia";
+            XmlDocument xades = new XmlDocument();
+            xades.Load(filename);
+            var namespaceId = new XmlNamespaceManager(xades.NameTable);
+            namespaceId.AddNamespace("ds", "http://www.w3.org/2000/09/xmldsig#");
+            namespaceId.AddNamespace("xades", "http://uri.etsi.org/01903/v1.3.2#");
+
+            //check Signature Id
+            XmlNode dsSignatureId = xades.SelectSingleNode("//ds:Signature", namespaceId).Attributes["Id"];
+            if (dsSignatureId == null)
+                return "ds:Signature neobsahuje Id atribut";
+
+            //check Signature namespace xmlns:ds
+            XmlNode dsSignatureXmlns = xades.SelectSingleNode("//ds:Signature", namespaceId).Attributes["xmlns:ds"];
+            if (dsSignatureXmlns == null)
+                return "ds:Signature nema specifikovany namespace xmlns:ds";
+
+            //check SignatureValue Id
+            XmlNode dsSignatureValueId = xades.SelectSingleNode("//ds:SignatureValue", namespaceId).Attributes["Id"];
+            if (dsSignatureValueId == null)
+                return "ds:SignatureValue neobsahuje Id atribut";
+
+            //check SignedInfo
+            XmlNode signedInfo = xades.SelectSingleNode("//ds:SignedInfo", namespaceId);
+            XmlNodeList dsKeyInfoId = signedInfo.SelectNodes("//ds:Reference", namespaceId);
+
+            if (dsKeyInfoId.Count < 1)
+                return "ds:SignedInfo neobsahuje ds:Reference";
+
+            String KeyInfo = "";
+            String SignatureProperties = "";
+            String SignedProperties = "";
+            List<string> Manifest = new List<string>();
+            foreach (XmlNode ReferenceList in dsKeyInfoId)
+            {
+                if (ReferenceList.Attributes["Id"] == null)
+                    continue;
+                else if(ReferenceList.Attributes["Id"] != null)
+                {
+                    if(ReferenceList.Attributes["Type"].Value.Contains("Object"))
+                    {
+                        KeyInfo = ReferenceList.Attributes["URI"].Value;
+                        KeyInfo = KeyInfo.Substring(1);
+                    }
+                    else if(ReferenceList.Attributes["Type"].Value.Contains("SignatureProperties"))
+                    {
+                        SignatureProperties = ReferenceList.Attributes["URI"].Value;
+                        SignatureProperties = SignatureProperties.Substring(1);
+                    }
+                    else if (ReferenceList.Attributes["Type"].Value.Contains("SignedProperties"))
+                    {
+                        SignedProperties = ReferenceList.Attributes["URI"].Value;
+                        SignedProperties = SignedProperties.Substring(1);
+                    }
+                    else if (ReferenceList.Attributes["Type"].Value.Contains("Manifest"))
+                    {
+                        String tmp = ReferenceList.Attributes["URI"].Value;
+                        tmp = tmp.Substring(1);
+                        Manifest.Add(tmp);
+                    }
+                }  
+            }
+
+            //use in element keyinfo
+            XmlNode ElementKeyInfo = xades.SelectSingleNode("//ds:KeyInfo", namespaceId);
+            XmlNode ElementSignatureProperties = xades.SelectSingleNode("//ds:SignatureProperties", namespaceId);
+            XmlNode ElementSignedProperties = xades.SelectSingleNode("//xades:SignedProperties", namespaceId);
+            if (ElementKeyInfo.Attributes["Id"] == null || !ElementKeyInfo.Attributes["Id"].Value.Equals(KeyInfo))
+                return "ds:Keyinfo nema Id alebo sa nezhoduje Id s URI";
+
+            if (ElementSignatureProperties.Attributes["Id"] == null || !ElementSignatureProperties.Attributes["Id"].Value.Equals(SignatureProperties))
+                return "ds:SignatureProperties nema Id alebo sa nezhoduje Id s URI";
+            
+            if (ElementSignedProperties.Attributes["Id"] == null || !ElementSignedProperties.Attributes["Id"].Value.Equals(SignedProperties))
+                return "xades:SignedProperties nema Id alebo sa nezhoduje Id s URI";
+
+            XmlNodeList ElementManifest = xades.SelectNodes("//ds:Manifest", namespaceId);
+            bool flag = false;
+            foreach(XmlNode OneManifest in ElementManifest)
+            {
+                foreach(String ManifestURI in Manifest)
+                {
+                    if (OneManifest.Attributes["Id"] == null || !OneManifest.Attributes["Id"].Value.Equals(ManifestURI))
+                        flag = true;
+                }
+            }
+
+            if(!flag)
+                return "ds:Manifest nema Id alebo sa nezhoduje Id s URI";
+
+            //check KeyInfo
+            if (ElementKeyInfo.Attributes["Id"] == null)
+                return "element ds:KeyInfo nema atribut Id";
+
+            XmlNode X509Data = ElementKeyInfo.SelectSingleNode("//ds:X509Data", namespaceId);
+            if (X509Data == null)
+                return "neobsahuje ds:X509Data";
+
+            if (X509Data.ChildNodes.Count < 3)
+                return "chybaju elementy z X509Data";
+
+            XmlNodeList elemList = X509Data.ChildNodes;
+            byte[] bytes;
+            var cert = new X509Certificate2();
+            String IssuerSerialFirst = "";
+            String IssuerSerialSecond = "";
+            String SubjectName = "";
+            for (int i = 0; i < elemList.Count; i++)
+            {
+                switch (elemList[i].Name)
+                {
+                    case "ds:X509Certificate":
+                        bytes = Convert.FromBase64String(elemList[i].InnerText);
+                        cert = new X509Certificate2(bytes);
+                        break;
+                    case "ds:X509IssuerSerial":
+                        if(elemList[i].HasChildNodes)
+                        {
+                            IssuerSerialFirst = elemList[i].FirstChild.InnerText;
+                            IssuerSerialSecond = elemList[i].LastChild.InnerText;
+                        }
+                        break;
+                    case "ds:X509SubjectName":
+                        SubjectName = elemList[i].InnerText;
+                        break;
+                }
+            }
+
+            if (!cert.Subject.Equals(SubjectName))
+                return "Subject sa nezhoduje";
+            if (!cert.Issuer.Equals(IssuerSerialFirst))
+                return "Issuername sa nezhoduje";
+            if (!cert.SerialNumber.Equals(IssuerSerialSecond))
+                return "Serialnumber sa nezhoduje";
+
+            return "OK";
         }
         
         private string step_4(string filename) //TODO: Overenie časovej pečiatky
